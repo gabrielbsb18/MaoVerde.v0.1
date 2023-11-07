d888f6fb60b96534453db92e34a62ebb65c31171
rayanemariade <141177599+rayanemariade@users.noreply.github.com>
2023-11-07 16:18:23 -0300

    adição da tela. close #28

--- src/screens/Estado/index.js
+++ src/screens/Estado/index.js
@@ -3,11 +3,22 @@ import { View, StyleSheet, Text, ImageBackground } from "react-native";
 import { Button } from "@rneui/themed";
 import { useNavigation } from "@react-navigation/native";
 import { Input } from "@rneui/themed";
+import { useForm, Controller } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const validationSchema = yup.object().shape({
+  estado: yup.string().required("Campo obrigatório"),
+});
 
 const Estado = () => {
   const navigation = useNavigation();
+  const { control, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(validationSchema),
+  });
 
-  const Contato = () => {
+  const handleContato = () => {
+  
     navigation.navigate("Contato");
   };
 
@@ -18,11 +29,23 @@ const Estado = () => {
     >
       <View style={styles.container}>
         <Text style={styles.text}>Qual estado você está disponível?</Text>
-        <Input
-          containerStyle={{ width: "85%"}}
-          style={{ color: "white" }}
-          placeholder="Digite seu estado!"
+        <Controller
+          control={control}
+          render={({ field: { value, onChange } }) => (
+            <Input
+              containerStyle={{ width: "85%" }}
+              style={{ color: "white" }}
+              placeholder="Digite seu estado!"
+              value={value}
+              onChangeText={onChange}
+            />
+          )}
+          name="estado"
+          defaultValue=""
         />
+        {errors.estado && (
+          <Text style={{ color: "red" }}>{errors.estado.message}</Text>
+        )}
         <Button
           title="Continuar"
           loading={false}
@@ -35,7 +58,7 @@ const Estado = () => {
           containerStyle={{
             width: 310,
           }}
-          onPress={() => Contato()}
+          onPress={handleSubmit(handleContato)}
         />
       </View>
     </ImageBackground>
