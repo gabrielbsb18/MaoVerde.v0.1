3839964db11aa3124df2eb0e4eeab3e482e03b64
gabrielbsb18 <142273716+gabrielbsb18@users.noreply.github.com>
2023-11-27 21:45:04 -0300

    Adiciona o botão de editar informações. close #48

--- src/screens/RevisarInformacoes/index.js
+++ src/screens/RevisarInformacoes/index.js
@@ -1,5 +1,12 @@
-import React, {useContext}from "react";
-import { View, Text, StyleSheet, Image, TextInput, ScrollView } from "react-native";
+import React, { useContext } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TextInput,
+  ScrollView,
+} from "react-native";
 
 import { useNavigation } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -7,49 +14,69 @@ import { TouchableOpacity } from "react-native";
 
 import {} from "../DetalhesCard";
 
-import {FormContext} from '../../contexts/form'
+import { FormContext } from "../../contexts/form";
 
 const FormParticipacao = () => {
-  const {user} =useContext(FormContext)
+  const { user } = useContext(FormContext);
   const navigation = useNavigation("");
 
   const handleNavConfirmado = () => {
     navigation.navigate("Confirmado");
   };
+  const handleNavBack = () => {
+    navigation.navigate("FormParticipacao")
+  } 
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
-      <View style={styles.buttonPerfil}>
-        <View style={{ justifyContent: "center" }}>
-          <Image source={require("../../../assets/capaParticipacao.png")} />
+        <View style={styles.buttonPerfil}>
+          <View style={{ justifyContent: "center" }}>
+            <Image source={require("../../../assets/capaParticipacao.png")} />
 
-          <Text style={styles.text}>Prevênção de </Text>
-          <Text style={styles.text}>Incêndio </Text>
+            <Text style={styles.text}>Prevênção de </Text>
+            <Text style={styles.text}>Incêndio </Text>
+          </View>
         </View>
-      </View>
-      <View style={styles.form}>
-        <Text style={{ color: "#fff", marginLeft: 14, fontSize: 15 }}>
-        Revise suas Informações
-        </Text>
-        <Text style={styles.textInput}>Nome Completo</Text>
-        <Text style={styles.input}>{user.nome}</Text>
-        <Text style={styles.textInput}>Número para Contato</Text>
-        <Text style={styles.input}>{user.tel}</Text>
-        <Text style={styles.textInput}>Número de Documento RG ou CPF</Text>
-        <Text style={styles.input}>{user.doc}</Text>
-        <Text style={styles.textInput}>Endereço Residencial </Text>
-        <Text style={styles.input}> {user.endereco}</Text>
+        <View style={styles.form}>
+          <Text style={{ color: "#fff", marginLeft: 14, fontSize: 15 }}>
+            Revise suas Informações
+          </Text>
+          <Text style={styles.textInput}>Nome Completo</Text>
+          <Text style={styles.input}>{user.nome}</Text>
+          <Text style={styles.textInput}>Número para Contato</Text>
+          <Text style={styles.input}>{user.tel}</Text>
+          <Text style={styles.textInput}>Número de Documento RG ou CPF</Text>
+          <Text style={styles.input}>{user.doc}</Text>
+          <Text style={styles.textInput}>Endereço Residencial </Text>
+          <Text style={styles.input}> {user.endereco}</Text>
 
-        <Text style={styles.textInput}>
-          Você tem alguma restrição dietética?
-        </Text>
-        <Text style={styles.input}>{user.restricao}</Text>
-      </View>
+          <Text style={styles.textInput}>
+            Você tem alguma restrição dietética?
+          </Text>
+          <Text style={styles.input}>{user.restricao}</Text>
+        </View>
 
-      
-      <TouchableOpacity style={styles.customButton} onPress={handleNavConfirmado}>
-        <Text style={styles.customButtonText}>Participar</Text>
-      </TouchableOpacity>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <TouchableOpacity
+            style={styles.customButton}
+            onPress={handleNavBack}
+          >
+            <Text style={styles.customButtonText}>Editar informações</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.customButton1}
+            onPress={handleNavConfirmado}
+          >
+            <Text style={styles.customButtonText}>Participar</Text>
+          </TouchableOpacity>
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
@@ -122,28 +149,36 @@ const styles = StyleSheet.create({
     marginBottom: 8,
     marginLeft: 14,
     marginTop: 15,
-    fontWeight:"bold",
-    alignSelf: 'stretch'
+    fontWeight: "bold",
+    alignSelf: "stretch",
   },
   input: {
     color: "white",
     paddingLeft: 8,
     height: 40,
     marginLeft: 14,
-    alignSelf: 'stretch'
+    alignSelf: "stretch",
   },
   form: {
-    alignSelf: 'stretch'
+    alignSelf: "stretch",
   },
   customButton: {
     backgroundColor: "green",
     borderRadius: 15,
     padding: 10,
     alignItems: "center",
-    marginTop: 35,
+    marginTop: 0,
     marginRight: 15,
-    marginBottom: 15,
-    alignSelf: 'flex-end'
+    marginBottom: 0,
+  },
+  customButton1: {
+    backgroundColor: "green",
+    borderRadius: 15,
+    padding: 10,
+    alignItems: "center",
+    marginTop: 0,
+    marginRight: 15,
+    marginBottom: 0,
   },
   customButtonText: {
     color: "#fff",
